ee87ab8379eca0413fa6b3c2818993cebdf2084a
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2024-09-25 12:29:42 +0200

    Added simple camera streaming script to check camera image

---
+++ tactile_image_processing/test_camera.py
@@ -0,0 +1,33 @@
+import cv2
+
+def main(source):
+    # Initialize the camera. 0 is typically the default camera, but you can change it if you have multiple cameras.
+    cap = cv2.VideoCapture(source)
+
+    # Check if the camera is opened correctly
+    if not cap.isOpened():
+        print("Error: Could not open video stream or file")
+        exit()
+
+    # Continuously capture frames from the camera
+    while True:
+        ret, frame = cap.read()
+
+        # If frame reading was successful
+        if ret:
+            # Display the resulting frame
+            cv2.imshow('Camera Stream', frame)
+            
+            # Exit if 'q' key is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            print("Error: Failed to capture frame")
+            break
+
+    # Release the camera and close any open windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(4)
